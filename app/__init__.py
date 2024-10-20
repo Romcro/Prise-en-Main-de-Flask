ed0b43a15fde65f98cb36fb1d0d8a87e2f60dd59
Romu Cro <licence@konekodesignstudio.fr>
2024-10-20 21:39:27 +0200

    wip

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates'))
 
 from app import routes
